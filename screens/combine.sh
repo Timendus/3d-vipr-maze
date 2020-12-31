3f44ef6b74155b5431a8fe744368714831f72404
Timendus <mail@timendus.com>
2020-12-31 14:33:08 +0100

    Refactor most of the stuff that deals with four colours

--- screens/combine.sh
+++ screens/combine.sh
@@ -7,58 +7,58 @@
 cat hall-0.txt > screens.txt
 
 # Outer variations
-sed -n -e 1,8p -e 25,33p hall-1.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p hall-2.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p hall-1.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p hall-2.txt >> screens.txt
 
 # Inner variations
-sed -n 9,25p hall-3.txt >> screens.txt
-sed -n 9,25p hall-3-5.txt >> screens.txt
-sed -n 9,25p hall-3-6.txt >> screens.txt
-sed -n 9,25p hall-4.txt >> screens.txt
-sed -n 9,25p hall-4-6.txt >> screens.txt
-sed -n 9,25p hall-5.txt >> screens.txt
-sed -n 9,25p hall-6.txt >> screens.txt
+sed -n 25,73p hall-3.txt >> screens.txt
+sed -n 25,73p hall-3-5.txt >> screens.txt
+sed -n 25,73p hall-3-6.txt >> screens.txt
+sed -n 25,73p hall-4.txt >> screens.txt
+sed -n 25,73p hall-4-6.txt >> screens.txt
+sed -n 25,73p hall-5.txt >> screens.txt
+sed -n 25,73p hall-6.txt >> screens.txt
 
 # Full base image
 cat wall-1.txt >> screens.txt
 
 # Outer variations
-sed -n -e 1,8p -e 25,33p wall-1-1.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-1-2.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-1-3.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-1-4.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-1-5.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-1-6.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-2-3.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-2-4.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-2-5.txt >> screens.txt
-sed -n -e 1,8p -e 25,33p wall-2-6.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-1-1.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-1-2.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-1-3.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-1-4.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-1-5.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-1-6.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-2-3.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-2-4.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-2-5.txt >> screens.txt
+sed -n -e 1,24p -e 73,96p wall-2-6.txt >> screens.txt
 
 # Inner variations
-sed -n 9,25p wall-2.txt >> screens.txt
-sed -n 9,25p wall-3.txt >> screens.txt
-sed -n 9,25p wall-3-3.txt >> screens.txt
-sed -n 9,25p wall-3-4.txt >> screens.txt
-sed -n 9,25p wall-3-5.txt >> screens.txt
-sed -n 9,25p wall-3-6.txt >> screens.txt
-sed -n 9,25p wall-4.txt >> screens.txt
-sed -n 9,25p wall-4-3.txt >> screens.txt
-sed -n 9,25p wall-4-4.txt >> screens.txt
-sed -n 9,25p wall-4-5.txt >> screens.txt
-sed -n 9,25p wall-4-6.txt >> screens.txt
-sed -n 9,25p wall-5.txt >> screens.txt
-sed -n 9,25p wall-5-3.txt >> screens.txt
-sed -n 9,25p wall-5-4.txt >> screens.txt
-sed -n 9,25p wall-5-5-3.txt >> screens.txt
-sed -n 9,25p wall-5-5.txt >> screens.txt
-sed -n 9,25p wall-5-6.txt >> screens.txt
-sed -n 9,25p wall-5-6-3.txt >> screens.txt
+sed -n 25,73p wall-2.txt >> screens.txt
+sed -n 25,73p wall-3.txt >> screens.txt
+sed -n 25,73p wall-3-3.txt >> screens.txt
+sed -n 25,73p wall-3-4.txt >> screens.txt
+sed -n 25,73p wall-3-5.txt >> screens.txt
+sed -n 25,73p wall-3-6.txt >> screens.txt
+sed -n 25,73p wall-4.txt >> screens.txt
+sed -n 25,73p wall-4-3.txt >> screens.txt
+sed -n 25,73p wall-4-4.txt >> screens.txt
+sed -n 25,73p wall-4-5.txt >> screens.txt
+sed -n 25,73p wall-4-6.txt >> screens.txt
+sed -n 25,73p wall-5.txt >> screens.txt
+sed -n 25,73p wall-5-3.txt >> screens.txt
+sed -n 25,73p wall-5-4.txt >> screens.txt
+sed -n 25,73p wall-5-5-3.txt >> screens.txt
+sed -n 25,73p wall-5-5.txt >> screens.txt
+sed -n 25,73p wall-5-6.txt >> screens.txt
+sed -n 25,73p wall-5-6-3.txt >> screens.txt
 
 # Sprites
-sed -n 1,29p sprites.txt >> screens.txt
-sed -n 9,25p viper4.txt >> screens.txt
-sed -n 9,25p viper-neck.txt >> screens.txt
-sed -n 25,33p sprites2.txt >> screens.txt
+sed -n 1,97p sprites.txt >> screens.txt
+sed -n 25,73p viper4.txt >> screens.txt
+sed -n 25,73p viper-neck.txt >> screens.txt
+sed -n 73,96p sprites2.txt >> screens.txt
 
 # Bitmaps
 cat map-victory.txt >> screens.txt
--- screens/convert.sh
+++ screens/convert.sh
@@ -2,6 +2,7 @@
 #
 # This script converts any PNG files (that use the right colours and dimensions)
 # in this directory to .txt files, using ImageMagick and some Bash magic.
+# (Adapted from 16x16 four colour version, so it may be a bit convoluted)
 
 for file in *
 do
@@ -13,34 +14,29 @@ do
       echo "Converting '$file'..."
       echo "" > "$filename"
 
-      for ((x=0;x<64;x+=16))
+      for ((x=0;x<64;x+=8))
       do
-        for ((y=0;y<32;y+=16))
+        for ((y=0;y<32;y+=8))
         do
           # Read in the source file as RGB values, one per line
-          values=`convert "$file" -crop 16x16+$x+$y rgb:- | xxd -ps | tr -d '\n' | fold -w6`
+          values=`convert "$file" -crop 8x8+$x+$y rgb:- | xxd -ps | tr -d '\n' | fold -w6`
 
-          # Convert RGB colours to two bit layers
+          # Convert RGB colours to one bit layer
           layer1=()
-          layer2=()
           for value in $values
           do
             case $value in
               662200)
-                layer1+='1'
-                layer2+='1'
+                layer1+='0'
                 ;;
               996601|795548)
                 layer1+='0'
-                layer2+='0'
                 ;;
               ff6602)
-                layer1+='0'
-                layer2+='1'
+                layer1+='1'
                 ;;
               ffcc01|ffc107)
                 layer1+='1'
-                layer2+='0'
                 ;;
               *)
                 echo "Unkown colour: $value"
@@ -49,16 +45,12 @@ do
 
           # Convert binary layers to hexadecimal values
           layer1=`echo $layer1 | fold -w8`
-          layer2=`echo $layer2 | fold -w8`
           layer1bytes=()
-          layer2bytes=()
           for value in $layer1; do layer1bytes+=`printf '0x%x ' "$((2#$value))"`; done
-          for value in $layer2; do layer2bytes+=`printf '0x%x ' "$((2#$value))"`; done
 
           # Output to file
           echo ": ${file%%.*}+$x+$y
     $layer1bytes
-    $layer2bytes
   " >> "$filename"
         done
       done
