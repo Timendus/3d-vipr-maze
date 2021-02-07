e7cc7d21a6fcb92d936595a08302a1cda585f5bf
Timendus <mail@timendus.com>
2021-02-07 21:44:31 +0100

    Inline rendering code, use uncompressed images for SCHIP version

--- build.sh
+++ build.sh
@@ -13,12 +13,21 @@ echo ":org 0x200" >> 3dvipermaze.8o
 echo >> 3dvipermaze.8o
 
 cat src/main.8o >> 3dvipermaze.8o
+
+echo "  #if VIP" >> 3dvipermaze.8o
 cat src/decompression.8o >> 3dvipermaze.8o
+echo "  #end" >> 3dvipermaze.8o
+
 cat src/map-management.8o >> 3dvipermaze.8o
 cat src/render-3d.8o >> 3dvipermaze.8o
 cat src/key-input.8o >> 3dvipermaze.8o
 
+echo "  #if VIP" >> 3dvipermaze.8o
 cat screens/compressed-screens.txt >> 3dvipermaze.8o
+echo "  #else" >> 3dvipermaze.8o
+cat screens/screens.txt >> 3dvipermaze.8o
+echo "  #end" >> 3dvipermaze.8o
+
 cat data/maps.8o >> 3dvipermaze.8o
 cat data/game-state.8o >> 3dvipermaze.8o
 cat data/binary-tree.8o >> 3dvipermaze.8o
