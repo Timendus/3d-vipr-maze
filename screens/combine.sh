23759adbb76f192f6da3389445ec31b98297ba06
Timendus <mail@timendus.com>
2021-04-28 15:03:28 +0200

    Redraw many of the screens so the boundary of hall-2 falls neatly between two columns

--- screens/combine.sh
+++ screens/combine.sh
@@ -8,27 +8,26 @@ cat hall-0.txt > screens.txt
 
 # Outer variations
 sed -n -e 1,4p -e 22,25p hall-1.txt >> screens.txt
-sed -n -e 1,7p -e 19,25p hall-2.txt >> screens.txt
+sed -n -e 4,7p -e 19,22p hall-2.txt >> screens.txt
 
 # Inner variations
 sed -n -e 7,10p -e 16,19p hall-3.txt >> screens.txt
 sed -n 7,19p hall-4.txt >> screens.txt
 sed -n 10,16p hall-5.txt >> screens.txt
 
-# Full base image
-cat wall-1.txt >> screens.txt
+# Semi-full base image
+sed -n 4,22p wall-1.txt >> screens.txt
 
 # Outer variations
-sed -n -e 1,4p -e 22,25p wall-1-1.txt >> screens.txt
 sed -n -e 1,4p -e 22,25p wall-1-2.txt >> screens.txt
 sed -n -e 1,4p -e 22,25p wall-1-3.txt >> screens.txt
 sed -n -e 1,4p -e 22,25p wall-1-4.txt >> screens.txt
-sed -n -e 1,4p -e 22,25p wall-1-5.txt >> screens.txt
-sed -n -e 1,4p -e 22,25p wall-1-6.txt >> screens.txt
-sed -n -e 1,7p -e 19,25p wall-2-3.txt >> screens.txt
-sed -n -e 1,7p -e 19,25p wall-2-4.txt >> screens.txt
-sed -n -e 1,7p -e 19,25p wall-2-5.txt >> screens.txt
-sed -n -e 1,7p -e 19,25p wall-2-6.txt >> screens.txt
+sed -n 1,4p wall-1-5.txt >> screens.txt
+sed -n 1,4p wall-1-6.txt >> screens.txt
+sed -n -e 4,7p -e 19,22p wall-2-3.txt >> screens.txt
+sed -n -e 4,7p -e 19,22p wall-2-4.txt >> screens.txt
+sed -n -e 4,7p -e 19,22p wall-2-5.txt >> screens.txt
+sed -n -e 4,7p -e 19,22p wall-2-6.txt >> screens.txt
 
 # Inner variations
 sed -n 7,19p wall-2.txt >> screens.txt
