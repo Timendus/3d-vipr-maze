34e5f6ff0dffd4f2941cfe49781383d759d0071a
Timendus <mail@timendus.com>
2021-04-28 16:10:40 +0200

    Make 'COMPRESSION' a flag, so we can have a VIP version without it

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ echo >> 3dvipermaze.8o
 
 cat src/main.8o >> 3dvipermaze.8o
 
-echo "  #if VIP" >> 3dvipermaze.8o
+echo "  #if COMPRESSION" >> 3dvipermaze.8o
 cat src/decompression.8o >> 3dvipermaze.8o
 echo "  #end" >> 3dvipermaze.8o
 
@@ -22,7 +22,7 @@ cat src/map-management.8o >> 3dvipermaze.8o
 cat src/render-3d.8o >> 3dvipermaze.8o
 cat src/key-input.8o >> 3dvipermaze.8o
 
-echo "  #if VIP" >> 3dvipermaze.8o
+echo "  #if COMPRESSION" >> 3dvipermaze.8o
 cat screens/compressed-screens.txt >> 3dvipermaze.8o
 echo "  #else" >> 3dvipermaze.8o
 cat screens/screens.txt >> 3dvipermaze.8o
@@ -35,6 +35,7 @@ cat data/binary-tree.8o >> 3dvipermaze.8o
 echo "# That's all folks!" >> 3dvipermaze.8o
 
 ./scripts/macros.js 3dvipermaze.8o bin/3dviprmaze-vip.8o VIP
+./scripts/macros.js 3dvipermaze.8o bin/3dviprmaze-vip-compression.8o VIP COMPRESSION
 ./scripts/macros.js 3dvipermaze.8o bin/3dviprmaze-schip.8o SCHIP
 rm 3dvipermaze.8o
 
@@ -44,5 +45,6 @@ cat bin/3dviprmaze-vip.8o | pbcopy
 # Build binaries using command line octo
 # Requires octo repository to have been cloned in the parent directory
 ../Octo/octo bin/3dviprmaze-vip.8o bin/3dviprmaze-vip.ch8
+../Octo/octo bin/3dviprmaze-vip-compression.8o bin/3dviprmaze-vip-compression.ch8
 ../Octo/octo bin/3dviprmaze-schip.8o bin/3dviprmaze-schip.ch8
 ../Octo/octo --options octo-html.json bin/3dviprmaze-vip.8o docs/index.html
